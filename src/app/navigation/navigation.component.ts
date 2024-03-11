5a42b14bc393b621c94c8d97b1257e057be5194f
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-03-11 20:22:10 +0100

    start navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatMenuModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
