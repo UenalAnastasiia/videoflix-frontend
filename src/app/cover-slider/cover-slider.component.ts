4ba569dc73c90c62839b47cfbbdb8be73af2cf50
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-03-19 16:35:45 +0100

    add slidershow

--- src/app/cover-slider/cover-slider.component.ts
+++ src/app/cover-slider/cover-slider.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -10,8 +10,29 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 })
 export class CoverSliderComponent implements OnInit {
   @Input() videos: any;
+  nextCover: number = 2;
+  currentCover = 1;
+  lastCover: number = 0;
+  showCover = true;
 
   ngOnInit() {
-    
+    this.slideCover();
+  }
+
+
+  slideCover() {
+    setInterval(() => {
+      this.currentCover++;
+      this.lastCover++;
+      this.nextCover++;
+      this.currentCover = this.currentCover % this.videos.length;
+      this.lastCover = this.lastCover % this.videos.length;
+      this.nextCover = this.nextCover % this.videos.length;
+      this.showCover = false;
+
+      setTimeout(() => {
+        this.showCover = true;
+      }, 100);
+    }, 5000);
   }
 }
